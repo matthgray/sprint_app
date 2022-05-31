110ab934be31e812f09702263d8a94f1c5bf1cbc
matthgray <matthewlearns2@gmail.com>
2022-05-30 20:20:05 -0500

    removed plt package

--- area_dashboard.py
+++ area_dashboard.py
@@ -1,7 +1,7 @@
 # needed packages
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.express as px
 import seaborn as sns
 import numpy as np
